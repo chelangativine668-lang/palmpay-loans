35a34045fad7f1ad7afa619c0884a85306264557
langat1081g <langat1081g@gmail.com>
2026-01-25 20:40:37 +0300

    Update bot tokens and chat IDs for multiple bots

--- server.js
+++ server.js
@@ -16,10 +16,16 @@ const requestBotMap = {};     // requestId -> botId
 const bots = [
     {
         botId: 'bot1',
-        botToken: process.env.TELEGRAM_BOT_TOKEN,
-        chatId: process.env.TELEGRAM_CHAT_ID
+        botToken: process.env.BOT1_TOKEN,
+        chatId: process.env.BOT1_CHATID
+    },
+    {
+        botId: 'bot2',
+        botToken: process.env.BOT2_TOKEN,
+        chatId: process.env.BOT2_CHATID
     }
 ];
+
 
 // ---------------- MIDDLEWARE ----------------
 app.use(express.json());
